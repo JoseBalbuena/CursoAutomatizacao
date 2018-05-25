2904972b6fcd10708ae475973930ae20e6e7d97e
JoseBalbuena <33149725+JoseBalbuena@users.noreply.github.com>
2018-05-25 11:56:59 -0400

    Create aristajsonrpc.py

---
+++ API/aristajsonrpc.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+from jsonrpclib import Server
+
+switch = Server( "http://admin:admin@172.16.1.2:8000/command-api" ) 
+response = switch.runCmds( 1, [ "show hostname" ] ) 
+print "Hello, my name is: ", response[0][ "hostname" ] 
+response = switch.runCmds( 1, [ "show version" ] ) 
+print "My MAC address is: ", response[0][ "systemMacAddress" ] 
+print "My version is: ", response[0][ "version" ]
+
